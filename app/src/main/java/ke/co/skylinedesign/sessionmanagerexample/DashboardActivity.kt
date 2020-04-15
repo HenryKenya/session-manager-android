9dff25e430a50fd94c2cb853e33ba413a86b0bef
HenryKenya <henry.onyango@outlook.com>
2020-04-15 17:31:03 +0300

    Add onclick listener to button for logging in user Create DashboardActivity for demo

---
+++ app/src/main/java/ke/co/skylinedesign/sessionmanagerexample/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package ke.co.skylinedesign.sessionmanagerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
--- app/src/main/java/ke/co/skylinedesign/sessionmanagerexample/MainActivity.kt
+++ app/src/main/java/ke/co/skylinedesign/sessionmanagerexample/MainActivity.kt
@@ -3,6 +3,7 @@ package ke.co.skylinedesign.sessionmanagerexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ke.co.skylinedesign.session_manager.SessionManagerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        button.setOnClickListener {
+            SessionManagerUtil.startUserSession(this, 60)
+        }
     }
 }
